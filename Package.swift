d823a24452c0d558220bc02f9ec05ae6b5dc1cbb
Jonathan Guthrie <jono@perfect.org>
2017-10-10 10:22:54 -0400

    update for swift 4

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // Generated automatically by Perfect Assistant Application
-// Date: 2017-04-01 20:14:44 +0000
+// Date: 2017-10-10 14:15:49 +0000
 import PackageDescription
 let package = Package(
-    name: "FileUploads",
-    targets: [],
-    dependencies: [
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 0),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 2),
-        .Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 2),
-        .Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 1),
-        .Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 1),
-    ]
+	name: "FileUploads",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/JSONConfig.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Mustache.git", majorVersion: 3),
+		.Package(url: "https://github.com/SwiftORM/StORM.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3),
+		.Package(url: "https://github.com/iamjono/SwiftString.git", majorVersion: 2),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-RequestLogger.git", majorVersion: 3),
+	]
 )
